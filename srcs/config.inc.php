08e74b62ba3ffcfd1d909d077b204fdb75724d93
mqueen21 <76485564+mqueen21@users.noreply.github.com>
2021-11-27 17:04:23 +0300

    Add files via upload

---
+++ srcs/config.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+$cfg['blowfish_secret'] = 'sffsdgggef24442t5tgfr32dgdg33333333';
+
+$i = 0;
+
+$i++;
+
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+
+$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
+
+$cfg['UploadDir'] = '';
+$cfg['SaveDir'] = '';
